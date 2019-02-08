0a0cf4f7953f62508bc7c2bf5e51ab30c28b493f
lsn4nk <lsn4nk1@gmail.com>
2019-02-09 00:45:06 +0200

    added server

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log('start');
+
+const http = require('http');
+const port = 3000;
+const url = '127.0.0.1'
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n')
+});
+
+server.listen(port, url, () => {
+    console.log(`Start listening on port ${port}`)
+});
+
+console.log('finish');
